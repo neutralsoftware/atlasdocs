620b10adfa0d6cdc4f678e2b216f3e9fa16d2cb8
Max Van den Eynde <maxvdec@gmail.com>
2026-09-06 19:25:05 +0200

    Added branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./global.css";
+import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 
-const inter = Inter({
-  subsets: ['latin'],
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
+
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+    <html lang="en" className={`${geist.className}`} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen font-sans">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
