59fa2364ebe05974f871e078e1980aefaad89ec0
Max Van den Eynde <maxvdec@gmail.com>
2026-09-06 20:13:13 +0200

    Added the options in the get started page

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,10 +1,19 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Tabs, Tab } from "fumadocs-ui/components/tabs";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
     ...components,
+
+    Step,
+    Steps,
+
+    Tabs,
+    Tab,
   } satisfies MDXComponents;
 }
 
