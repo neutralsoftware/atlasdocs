81f4b10a808780b4582ac4f5e2ba8c723cdd7cc1
Max Van den Eynde <maxvdec@gmail.com>
2026-09-08 13:57:31 +0200

    Added a new 'what is atlas' page

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -2,23 +2,32 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 import { Step, Steps } from "fumadocs-ui/components/steps";
-import { Tabs, Tab } from "fumadocs-ui/components/tabs";
-
-export function getMDXComponents(components?: MDXComponents) {
-  return {
-    ...defaultMdxComponents,
-    ...components,
-
-    Step,
-    Steps,
-
+import {
     Tabs,
     Tab,
-  } satisfies MDXComponents;
+    TabsList,
+    TabsTrigger,
+    TabsContent,
+} from "fumadocs-ui/components/tabs";
+
+export function getMDXComponents(components?: MDXComponents) {
+    return {
+        ...defaultMdxComponents,
+        ...components,
+
+        Step,
+        Steps,
+
+        Tabs,
+        Tab,
+        TabsList,
+        TabsTrigger,
+        TabsContent,
+    } satisfies MDXComponents;
 }
 
 export const useMDXComponents = getMDXComponents;
 
 declare global {
-  type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
+    type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
 }
